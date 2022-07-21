0654aeadcd1c180bd927a7484e36a3906b31defe
Glaucia Santana <glaucia.sstana@live.com>
2022-07-21 20:21:18 -0300

    Exercicio 01 C# completo Finalizado

---
+++ Exercicio/Entites/Order.cs
@@ -0,0 +1,23 @@
+﻿using Exercicio.Entites.Enums;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercicio.Entites{
+    internal class Order{
+        public int Id { get; set; }
+            public DateTime Moment { get; set; }
+            public OrderStatus Status { get; set; }
+
+            public override string ToString()
+            {
+                return Id
+                    + ", "
+                    + Moment
+                    + ", "
+                    + Status;
+            }
+    }
+}
